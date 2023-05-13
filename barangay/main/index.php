f991dde84ee73a7d69f572c09842fcd9d7f7ac89
georenchlg <94753197+georeeen@users.noreply.github.com>
2023-05-13 16:32:48 +0800

    Update index.php

--- barangay/main/index.php
+++ barangay/main/index.php
@@ -21,8 +21,8 @@
         display:none;
     }
     .dataTables_filter input { 
-    padding-top: 20px;
-    padding-bottom: 20px;}
+    padding-top: 50px;
+    padding-bottom: 50px;}
     </style>
 </head>
 <body>
@@ -43,59 +43,48 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
-        <li><a href="about.php">About</a></li>
         <li><a href="../login.php">Admin</a></li>
         <li><a href="../pages/resident/login.php">Resident</a></li>
-        <li><a href="../pages/zone/login.php">Zone Leader</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
 
 <div class="wrapper row-offcanvas row-offcanvas-left">
-<div class="container-fluid">
-<table id="table" class="table table-bordered table-striped">
-    <thead>
-        <tr>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Barangay</th>
-            <th>Zone</th>
-            <th>Age</th>
-            <th>Gender</th>
-            <th>Former Address</th>
-            <th style="width: 5% !important;">Option</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        include "../pages/connection.php";
-        $squery = mysqli_query($con, "SELECT *,CONCAT(lname, ', ', fname, ' ', mname) as cname FROM tblresident");
-        while($row = mysqli_fetch_array($squery))
-        {
-            echo '
-            <tr>
-                <td style="width:70px;"><image src="../pages/resident/image/'.basename($row['image']).'" style="width:60px;height:60px;"/></td>
-                <td>'.$row['cname'].'</td>
-                <td>'.$row['barangay'].'</td>
-                <td>'.$row['zone'].'</td>
-                <td>'.$row['age'].'</td>
-                <td>'.$row['gender'].'</td>
-                <td>'.$row['formerAddress'].'</td>
-                <td><button class="btn btn-primary btn-sm" data-target="#detailsModal'.$row['id'].'" data-toggle="modal"><i class="fa fa-search" aria-hidden="true"></i> Details</button></td>
-            </tr>
-            ';
-            include "detailsModal.php";
-        }
-        ?>
-    </tbody>
-</table>
 
+<div class="breadcrumb">
+    <h3>Welcome to About Page</h3>
 </div>
+
 </div>
 
 </body>
 
+
+<script src="../js/alert.js" type="text/javascript"></script>
+<script src="../js/bootstrap.min.js" type="text/javascript"></script>
+
+
+<script src="../js/morris/raphael-2.1.0.min.js" type="text/javascript"></script>
+<script src="../js/morris/morris.js" type="text/javascript"></script>
+<script src="../js/select2.full.js" type="text/javascript"></script>
+
+<script src="../js/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="../js/dataTables.buttons.min.js" type="text/javascript"></script>
+<script src="../js/buttons.print.min.js" type="text/javascript"></script>
+
+<script src="../js/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
+<!-- AdminLTE App -->
+<script src="../js/AdminLTE/app.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+  $(function() {
+      $("#table").dataTable({
+         "aoColumnDefs": [ { "bSortable": false, "aTargets": [ 0,5 ] } ],"aaSorting": []
+      });
+  });
+</script>
+</html>
 
 <script src="../js/alert.js" type="text/javascript"></script>
 <script src="../js/bootstrap.min.js" type="text/javascript"></script>
@@ -123,7 +112,7 @@
 
   $(document).ready(function () {             
   $('.dataTables_filterinput[type="search"]').css(
-     {'width':'350px','display':'inline-block'}
+     {'width':'5000px','display':'inline-block'}
   );
 });
 </script>
