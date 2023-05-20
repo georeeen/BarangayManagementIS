8ff8668ed09aefff5866b473d0082c4028601f01
georenchlg <94753197+georeeen@users.noreply.github.com>
2023-05-20 18:03:15 +0800

    Update edit_modal.php

--- barangay/pages/announcements/edit_modal.php
+++ barangay/pages/announcements/edit_modal.php
@@ -1,4 +1,4 @@
-<?php echo '<div id="editModal'.$row['id'].'" class="modal fade">
+<?php echo '<div id="editModal'.$row['announcement'].'" class="modal fade">
 <form method="post">
   <div class="modal-dialog modal-sm" style="width:300px !important;">
     <div class="modal-content">
@@ -9,18 +9,14 @@
         <div class="modal-body">
         <div class="row">
             <div class="col-md-12">
-                <input type="hidden" value="'.$row['id'].'" name="hidden_id" id="hidden_id"/>
+                <input type="hidden" value="'.$row['announcement'].'" name="hidden_id" id="hidden_id"/>
                 <div class="form-group">
-                    <label>Name: <span style="color:gray; font-size: 10px;">(Lastname, Firstname Middlename)</span></label>
-                    <input name="txt_edit_name" class="form-control input-sm" type="text" value="'.$row['name'].'"/>
+                    <label>Name: <span style="color:gray; font-size: 10px;">(Title)</span></label>
+                    <input name="txt_edit_announcement" class="form-control input-sm" type="text" value="'.$row['announcement'].'"/>
                 </div>
                 <div class="form-group">
                     <label>Username: </label>
-                    <input name="txt_edit_uname" class="form-control input-sm" type="text" value="'.$row['username'].'" />
-                </div>
-                <div class="form-group">
-                    <label>Password: </label>
-                    <input name="txt_edit_pass" class="form-control input-sm" type="password" value="'.$row['password'].'" />
+                    <input name="txt_edit_about" class="form-control input-sm" type="text" value="'.$row['about'].'" />
                 </div>
             </div>
         </div>
