f7f58323334dcdfa50693f95d1fef6c99d968d4f
georenchlg <94753197+georeeen@users.noreply.github.com>
2023-05-20 18:03:09 +0800

    Update announcements.php

--- barangay/pages/announcements/announcements.php
+++ barangay/pages/announcements/announcements.php
@@ -28,7 +28,7 @@
                 <!-- Content Header (Page header) -->
                 <section class="content-header">
                     <h1>
-                        Announcements/Events
+                        Announcements
                     </h1>
                     
                 </section>
@@ -39,10 +39,11 @@
                         <!-- left column -->
                             <div class="box">
                                 <div class="box-header">
-                                    <div style="padding:10px;">
-                                        
-                                        <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#addModal"><i class="fa fa-user-plus" aria-hidden="true"></i> Add Staff</button>  
-                                        <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button> 
+                                    <div style="padding:10px;" div class="pull-right">
+
+                                         <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#addModal"><i class="fa fa-plus" aria-hidden="true"></i> Create</button>  
+                                        <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal"><i class="fa fa-trash-o" aria-hidden="true"></i> Remove</button> 
+                                    
                                     </div>                                
                                 </div><!-- /.box-header -->
                                 <div class="box-body table-responsive">
@@ -51,22 +52,22 @@
                                         <thead>
                                             <tr>
                                                 <th style="width: 20px !important;"><input type="checkbox" name="chk_delete[]" class="cbxMain" onchange="checkMain(this)"/></th>
-                                                <th>Name</th>
-                                                <th>Username</th>
-                                                <th style="width: 40px !important;">Option</th>
+                                                <th>Title of Event</th>
+                                                <th>About</th>
+                                                <th style="width: 40px !important;">Manage</th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             <?php
-                                            $squery = mysqli_query($con, "select * from tblstaff");
+                                            $squery = mysqli_query($con, "select * from tblannounce");
                                             while($row = mysqli_fetch_array($squery))
                                             {
                                                 echo '
                                                 <tr>
-                                                    <td><input type="checkbox" name="chk_delete[]" class="chk_delete" value="'.$row['id'].'" /></td>
-                                                    <td>'.$row['name'].'</td>
-                                                    <td>'.$row['username'].'</td>
-                                                    <td><button class="btn btn-primary btn-sm" data-target="#editModal'.$row['id'].'" data-toggle="modal"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></td>
+                                                    <td><input type="checkbox" name="chk_delete[]" class="chk_delete" value="'.$row['announcement'].'" /></td>
+                                                    <td>'.$row['announcement'].'</td>
+                                                    <td>'.$row['about'].'</td>
+                                                    <td><button class="btn btn-primary btn-sm" data-target="#editModal'.$row['announcement'].'" data-toggle="modal"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></td>
                                                 </tr>
                                                 ';
 
