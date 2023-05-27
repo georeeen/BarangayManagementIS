e777b90bca3355f296d8b57254c64d20695568df
kylebrent <94148812+kylebrent@users.noreply.github.com>
2023-05-27 15:00:12 +0800

    Update index.php

--- barangay/main/index.php
+++ barangay/main/index.php
@@ -58,6 +58,7 @@
 <div class="breadcrumb">
   <center> <h3> <b>Welcome Resident,</b>  </h3> 
   &nbsp; 
+  <a class="navbar-brand" href="index.php"><img alt="Brand" src="../img/logo.png" style="width:50px; margin-top:-15px; "></a>
   <center>
  <a class="center" href="index.php"><img alt="Brand" src="../img/bg.jpg" style="width:800px; margin-top:50px;"></a>
   &nbsp; 
