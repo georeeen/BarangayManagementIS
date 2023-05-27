c157c47e0a441a047ce508dc5fe32bf025e09a50
kylebrent <94148812+kylebrent@users.noreply.github.com>
2023-05-27 13:54:06 +0800

    Update index.php

--- barangay/main/index.php
+++ barangay/main/index.php
@@ -58,7 +58,8 @@
  <t> </t>
 
  <p> {
-  text-indent: 50px;
+  
+  
 } <right> <h4> Barangay Tangos is a former fishpond that is part of the San Roque coast. The word Tangos means "sea coast" or "beach". The Patron of this barangay is San Roque which is believed by many to provide abundant marine resources to fishermen; which raises the livelihood of the village.
 
 Fishermen entrepreneurs usually lived in Barangay Tangos, which was once considered the "first class subdivision" of the BNI Realty Corporation. (BNI) because of their prosperous business.
