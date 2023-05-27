5581f9245249e02e66e690f98207612133cf2969
kylebrent <94148812+kylebrent@users.noreply.github.com>
2023-05-27 14:54:56 +0800

    Update index.php

--- barangay/main/index.php
+++ barangay/main/index.php
@@ -72,9 +72,9 @@
   began to be called Barangay Tangos.</p> </h4>
   
   <br> 
-  <br> <h4><p style="padding: 10px; border: 2px solid green;"> <b> VISION </b> </p>
+  <br> <h4><p style="padding: 10px; border: 2px solid green;"> <b> VISION </b> 
   <br>
-  <br> To be a community that promotes peace and wellness, environmental and health hygiene, become developed in the field of economy and provide more knowledge. Education for the youth towards the growth of the individual and the society as a whole.
+  <br> To be a community that promotes peace and wellness, environmental and health hygiene, become developed in the field of economy and provide more knowledge. Education for the youth towards the growth of the individual and the society as a whole. </p>
 
   <br> 
   <br>
