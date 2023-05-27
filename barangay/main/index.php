973429d6e8e3b2643f66a917be135a5c13d58087
kylebrent <94148812+kylebrent@users.noreply.github.com>
2023-05-27 15:11:02 +0800

    Update index.php

--- barangay/main/index.php
+++ barangay/main/index.php
@@ -29,7 +29,10 @@
     </style>
 </head>
 <body>
-background-image: img alt="Brand" src="../img/logo.png"
+{
+  background-image: url("paper.gif");
+  background-color: #cccccc;
+}
 <nav class="navbar navbar-inverse" style="border-radius:0px;">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
