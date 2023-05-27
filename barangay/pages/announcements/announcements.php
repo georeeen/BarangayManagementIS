9b8b17b0a9aeb732727f3af3d29e8d8cf1c4a4d7
kylebrent <94148812+kylebrent@users.noreply.github.com>
2023-05-27 13:12:01 +0800

    Update announcements.php

--- barangay/pages/announcements/announcements.php
+++ barangay/pages/announcements/announcements.php
@@ -28,7 +28,7 @@
                 <!-- Content Header (Page header) -->
                 <section class="content-header">
                     <h1>
-                        Announcements
+                        Announcement
                     </h1>
                     
                 </section>
