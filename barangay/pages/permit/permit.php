db4f4a87e3bdf691751af2402d57d9cd076d3e97
kylebrent <94148812+kylebrent@users.noreply.github.com>
2023-05-30 23:45:26 +0800

    Update permit.php

--- barangay/pages/permit/permit.php
+++ barangay/pages/permit/permit.php
@@ -47,12 +47,12 @@
                                 <div class="box-header">
                                     <div style="padding:10px;" div class= "pull-right">
                                         
-                                        <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#addModal"><i class="fa fa-user-plus" aria-hidden="true"></i> Add Permit</button>  
+                                        <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#addModal" style="background-color: green"><i class="fa fa-user-plus" aria-hidden="true"></i> Add Permit</button>  
                                         <?php 
                                             if(!isset($_SESSION['staff']))
                                             {
                                         ?>
-                                        <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal"><i class="fa fa-trash-o" aria-hidden="true"></i> Remove</button> 
+                                        <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal" style="background-color: red"><i class="fa fa-trash-o" aria-hidden="true"></i> Remove</button> 
                                         <?php
                                             }
                                         ?>
