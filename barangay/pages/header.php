eab392ec1eb11f1c4b84e30665b1898249ec6f5e
kylebrent <94148812+kylebrent@users.noreply.github.com>
2023-05-30 22:35:53 +0800

    Update header.php

--- barangay/pages/header.php
+++ barangay/pages/header.php
@@ -3,7 +3,7 @@
    echo  '<header class="header">
             <a href="#" class="logo">
                 <!-- Add the class icon to your logo image or logo icon to add the margining -->
-                Barangay Tangos North
+                Barangay Tangos
             </a>
             <!-- Header Navbar: style can be found in header.less -->
             <nav class="navbar navbar-static-top" role="navigation">
