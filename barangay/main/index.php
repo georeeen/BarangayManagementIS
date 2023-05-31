bdf9ed057f97abfe668974ca3d124910c1376235
lalay29 <94664141+lalay29@users.noreply.github.com>
2023-05-31 22:15:55 +0800

    Update index.php

--- barangay/main/index.php
+++ barangay/main/index.php
@@ -17,6 +17,11 @@
     <link href="../css/select2.css" rel="stylesheet" type="text/css" />
     <script src="../js/jquery-1.12.3.js" type="text/javascript"></script>
     <style>
+      body {
+  background-image: url("bg1.JPG");
+  background-repeat: repeat;
+  background-color: #cccccc;
+    }
 
     .no-print{
         display:none;
