fc4c84fab30ff3c7d3f6eba8c050ec176af5c8f8
kylebrent <94148812+kylebrent@users.noreply.github.com>
2023-06-01 00:17:27 +0800

    Revert "Update index.php"
    This reverts commit dbfffbe0da6ebfeb979cf38af680c8f0c7e831be.

--- barangay/main/index.php
+++ barangay/main/index.php
@@ -92,8 +92,8 @@
 
 
 </div>
-</style> 
-</body>   
+</body>
+</style>    
 
 <script src="../js/alert.js" type="text/javascript"></script>
 <script src="../js/bootstrap.min.js" type="text/javascript"></script>
