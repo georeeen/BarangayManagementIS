a1f9aa421198cd9676710c5739e497162f85c2be
lalay29 <94664141+lalay29@users.noreply.github.com>
2023-05-31 23:39:45 +0800

    Update index.php

--- barangay/main/index.php
+++ barangay/main/index.php
@@ -18,6 +18,10 @@
     <link href="../css/select2.css" rel="stylesheet" type="text/css" />
     <script src="../js/jquery-1.12.3.js" type="text/javascript"></script>
     <style>
+         body {
+  background-color: coral;
+  }
+
    
    .no-print{
         display:none;
