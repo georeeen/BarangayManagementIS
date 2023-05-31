7fffe3580c5abfad420254a18edc00857ddcefc2
georenchlg <94753197+georeeen@users.noreply.github.com>
2023-06-01 00:06:44 +0800

    Create dashboardresi.php

---
+++ barangay/pages/dashboardresi/dashboardresi.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+    <?php
+    session_start();
+    if(!isset($_SESSION['role']))
+    {
+        header("Location: ../../login.php"); 
+    }
+    else
+    {
+    ob_start();
+    include('../head_css.php'); ?>
+    <body class="skin-black">
+        <!-- header logo: style can be found in header.less -->
+        <?php 
+        
+        include "../connection.php";
+        ?>
+        <?php include('../header.php'); ?>
+
+        <div class="wrapper row-offcanvas row-offcanvas-left">
+            <!-- Left side column. contains the logo and sidebar -->
+            <?php include('../sidebar-left.php'); ?>
+
+            <!-- Right side column. Contains the navbar and content of the page -->
+            <aside class="right-side">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <h1>
+                        Dashboard
+                    </h1>
+                    
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+                    <div class="row">
+                        <!-- left column -->
+                            <div class="box">
+                                
+                                <div class="col-md-3 col-sm-6 col-xs-12"><br>
+                                  <div class="info-box">
+                                    <a href="../permit/permit.php"><span class="info-box-icon bg-green"><i class="fa fa-users"></i></span></a>
+
+                                    <div class="info-box-content"> 
+                                      <span class="info-box-text">Request Permit</span>
+                                      <span class="info-box-number">
+                                        <?php
+                                            $q = mysqli_query($con,"SELECT * from tblpermit");
+                                            $num_rows = mysqli_num_rows($q);
+                                            echo $num_rows;
+                                        ?>
+                                      </span>
+                                    </div>
+                                    <!-- /.info-box-content -->
+                                  </div>
+                                  <!-- /.info-box -->
+                                </div>
+        <!-- jQuery 2.0.2 -->
+        <?php }
+        include "../footer.php"; ?>
+<script type="text/javascript">
+    $(function() {
+        $("#table").dataTable({
+           "aoColumnDefs": [ { "bSortable": false, "aTargets": [ 0,5 ] } ],"aaSorting": []
+        });
+    });
+</script>
+    </body>
+</html>
