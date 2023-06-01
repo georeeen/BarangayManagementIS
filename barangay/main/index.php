e5dcc20981cd8b94877925762866ad89c27d13c5
lalay29 <94664141+lalay29@users.noreply.github.com>
2023-06-02 00:16:02 +0800

    Update index.php

--- barangay/main/index.php
+++ barangay/main/index.php
@@ -17,10 +17,13 @@
     <link href="../css/select2.css" rel="stylesheet" type="text/css" />
     <script src="../js/jquery-1.12.3.js" type="text/javascript"></script>
     <style>
+      p.a {
+  text-align-last: justify;
+      }
       h4 {
   color: white;
   text-shadow: 1px 1px 2px black, 0 0 25px black, 0 0 5px black;
-}
+  }
         display:none;
     }
     .dataTables_filter input { 
@@ -68,11 +71,12 @@
   &nbsp;
 <div> &nbsp; 
       &nbsp; </div> 
+      <p class= "a">
       <h4> <p style="padding: 20px; border: 4px solid green;">  Barangay Tangos </i> </b> is a former fishpond that is part of the San Roque coast. The word Tangos means "sea coast" or "beach". 
   The Patron of this barangay is San Roque which is believed by many to provide abundant marine resources to fishermen; which raises the livelihood of the village. Fishermen 
   entrepreneurs usually lived in Barangay Tangos, which was once considered the "first class subdivision" of the BNI Realty Corporation. (BNI) because of their prosperous 
   business. The village later became a "commercial" and "industrial" area and was called the "business capital" of Navotas. Livelihood improved in the village and in 1967 it 
-  began to be called Barangay Tangos.</p> </h4>
+  began to be called Barangay Tangos.</p> </h4> </p>
 
   <br> 
   <br> <h4><p style="padding: 10px; border: 2px solid green;"> <b> VISION </b> </p>
