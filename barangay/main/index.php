9be22edd208853c6f0b18aa2df8840a539c3657c
lalay29 <94664141+lalay29@users.noreply.github.com>
2023-06-02 14:30:17 +0800

    Update index.php

--- barangay/main/index.php
+++ barangay/main/index.php
@@ -65,6 +65,7 @@
   <a class="center" href="index.php"><img alt="Brand" src="../img/logo.png" style="width:70px; margin-top:-15px;"></a>
   <center>
  <a class="center" href="index.php"><img alt="Brand" src="mayor.jpg" style="width:400px; margin-top:50px; border: 2px solid gray;"></a>
+ <h3> <b>Mayor. John Reynald M. Tiangco</b>  </h3>
   &nbsp; 
   &nbsp;
 <div> &nbsp; 
