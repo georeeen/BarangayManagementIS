5ede370718c95b601f24a5d07c23bc964f27a348
georenchlg <94753197+georeeen@users.noreply.github.com>
2023-06-02 23:31:10 +0800

    Update announcements.php

---
+++ barangay/pages/announcements/announcements.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+
+    <?php
+    session_start();
+    if(!isset($_SESSION['role']))
+    {
+        header("Location: ../../login.php"); 
+    }
+    else
+    {
+    ob_start();
+    include('../head_css.php'); ?>
+    <body class="skin-black">
+        <!-- header logo: style can be found in header.less -->
+        <?php 
+        
+        include "../connection.php";
+        ?>
+        <?php include('../header.php'); ?>
+
+        <div class="wrapper row-offcanvas row-offcanvas-left">
+            <!-- Left side column. contains the logo and sidebar -->
+            <?php include('../sidebar-left.php'); ?>
+
+            <!-- Right side column. Contains the navbar and content of the page -->
+            <aside class="right-side">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                 <h1>
+                    Announcement </h1>
+                    
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+                    <div class="row">
+                        <!-- left column -->
+                            <div class="box">
+                                <div class="box-header">
+                                    <div style="padding:10px;" div class="pull-right">
+
+                                         <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#addModal" style="background-color: green"><i class="fa fa-plus" aria-hidden="true"></i> Create</button>  
+                                        <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal" style="background-color: red"><i class="fa fa-trash-o" aria-hidden="true"></i> Remove</button> 
+                                    
+                                    </div>                                
+                                </div><!-- /.box-header -->
+                                <div class="box-body table-responsive">
+                                <form method="post">
+                                    <table id="table" class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th style="width: 20px !important;"><input type="checkbox" name="chk_delete[]" class="cbxMain" onchange="checkMain(this)"/></th>
+                                                <th>Title of Event</th>
+                                                <th>About</th>
+                                                <th style="width: 40px !important;">Manage</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $squery = mysqli_query($con, "select * from tblannounce");
+                                            while($row = mysqli_fetch_array($squery))
+                                            {
+                                                echo '
+                                                <tr>
+                                                    <td><input type="checkbox" name="chk_delete[]" class="chk_delete" value="'.$row['announcement'].'" /></td>
+                                                    <td>'.$row['announcement'].'</td>
+                                                    <td>'.$row['about'].'</td>
+                                                    <td><button class="btn btn-primary btn-sm" data-target="#editModal'.$row['announcement'].'" data-toggle="modal" ><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></td>
+                                                </tr>
+                                                ';
+
+                                                include "edit_modal.php";
+                                            }
+                                            ?>
+                                        </tbody>
+                                    </table>
+
+                                    <?php include "../deleteModal.php"; ?>
+
+                                    </form>
+                                </div><!-- /.box-body -->
+                            </div><!-- /.box -->
+
+                            <?php include "../edit_notif.php"; ?>
+
+                            <?php include "../added_notif.php"; ?>
+
+                            <?php include "../delete_notif.php"; ?>
+
+                            <?php include "../duplicate_error.php"; ?>
+
+                            <?php include "add_modal.php"; ?>
+
+                            <?php include "edit_modal.php"; ?>    
+
+
+                    </div>   <!-- /.row -->
+                </section><!-- /.content -->
+            </aside><!-- /.right-side -->
+        </div><!-- ./wrapper -->
+        <!-- jQuery 2.0.2 -->
+        <?php }
+        include "../footer.php"; ?>
+<script type="text/javascript">
+    $(function() {
+        $("#table").dataTable({
+           "aoColumnDefs": [ { "bSortable": false, "aTargets": [ 0,3 ] } ],"aaSorting": []
+        });
+    });
+</script>
+    </body>
+</html>
