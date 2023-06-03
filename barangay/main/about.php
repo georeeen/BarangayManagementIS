876dc2f794270758e75db8399db02c558668903a
lalay29 <94664141+lalay29@users.noreply.github.com>
2023-06-03 13:11:38 +0800

    Update about.php

--- barangay/main/about.php
+++ barangay/main/about.php
@@ -22,7 +22,7 @@
 <nav class="navbar navbar-inverse" style="border-radius:0px;">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header" color="green">
+    <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
@@ -34,7 +34,7 @@
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
+<ul class="nav navbar-nav" background image= "green">
         <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
         <li><a href="about.php">About</a></li>
         <li><a href="../login.php">Admin</a></li>
