98b0b5ff6335b03944a7188a5f43ced395049a6e
lalay29 <94664141+lalay29@users.noreply.github.com>
2023-06-03 11:51:56 +0800

    Update index.php

--- barangay/main/index.php
+++ barangay/main/index.php
@@ -44,7 +44,7 @@
 <nav class="navbar navbar-inverse" style="border-radius:0px;">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
+    <div class="navbar-header" >
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
