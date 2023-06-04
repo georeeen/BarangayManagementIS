e16d830f843f52921ad5186478bd4791afbbdf2f
georenchlg <94753197+georeeen@users.noreply.github.com>
2023-06-04 08:52:56 +0800

    Update edit_modal.php

--- barangay/pages/household/edit_modal.php
+++ barangay/pages/household/edit_modal.php
@@ -4,7 +4,7 @@
     <div class="modal-content">
         <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h4 class="modal-title">Edit Course Info</h4>
+            <h4 class="modal-title">Edit Household</h4>
         </div>
         <div class="modal-body">
         <div class="row">
