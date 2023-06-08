f77914453219927a99829d5910552a1fff755766
georenchlg <94753197+georeeen@users.noreply.github.com>
2023-06-09 00:38:05 +0800

    Update donut-chart.php

--- barangay/pages/report/donut-chart.php
+++ barangay/pages/report/donut-chart.php
@@ -3,65 +3,16 @@
 	Morris.Donut({
 		element: 'morris-donut-chart',
 		data: [{
-			label: "No schooling completed",
+			label: "Male",
 			value: <?php 
-					$q = mysqli_query($con,"SELECT * from tblresident where highestEducationalAttainment = 'No schooling completed' ");
+					$q = mysqli_query($con,"SELECT * from tblresident where gender = 'Male' ");
 					$numrow = mysqli_num_rows($q);
 					echo $numrow;
 			 	?>
 		}, {
-			label: "Elementary",
+			label: "Female",
 			value: <?php 
-					$q = mysqli_query($con,"SELECT * from tblresident where highestEducationalAttainment = 'Elementary' ");
-					$numrow = mysqli_num_rows($q);
-					echo $numrow;
-			 	?>
-		}, {
-			label: "High school, undergrad",
-			value: <?php 
-					$q = mysqli_query($con,"SELECT * from tblresident where highestEducationalAttainment = 'High school, undergrad' ");
-					$numrow = mysqli_num_rows($q);
-					echo $numrow;
-			 	?>
-		}, {
-			label: "High school graduate",
-			value: <?php 
-					$q = mysqli_query($con,"SELECT * from tblresident where highestEducationalAttainment = 'High school graduate' ");
-					$numrow = mysqli_num_rows($q);
-					echo $numrow;
-			 	?>
-		}, {
-			label: "College, undergrad",
-			value: <?php 
-					$q = mysqli_query($con,"SELECT * from tblresident where highestEducationalAttainment = 'College, undergrad' ");
-					$numrow = mysqli_num_rows($q);
-					echo $numrow;
-			 	?>
-		}, {
-			label: "Vocational",
-			value: <?php 
-					$q = mysqli_query($con,"SELECT * from tblresident where highestEducationalAttainment = 'Vocational' ");
-					$numrow = mysqli_num_rows($q);
-					echo $numrow;
-			 	?>
-		}, {
-			label: "Bachelor's degree",
-			value: <?php 
-					$q = mysqli_query($con,"SELECT * from tblresident where highestEducationalAttainment = 'Bachelor''s degree' ");
-					$numrow = mysqli_num_rows($q);
-					echo $numrow;
-			 	?>
-		}, {
-			label: "Master's degree",
-			value: <?php 
-					$q = mysqli_query($con,"SELECT * from tblresident where highestEducationalAttainment = 'Master''s degree' ");
-					$numrow = mysqli_num_rows($q);
-					echo $numrow;
-			 	?>
-		}, {
-			label: "Doctorate degree",
-			value: <?php 
-					$q = mysqli_query($con,"SELECT * from tblresident where highestEducationalAttainment = 'Doctorate degree' ");
+					$q = mysqli_query($con,"SELECT * from tblresident where gender = 'Female' ");
 					$numrow = mysqli_num_rows($q);
 					echo $numrow;
 			 	?>
