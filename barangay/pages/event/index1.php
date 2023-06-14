e4b3b5484e50abca00117ad6d55f0cf5de18f90c
georenchlg <94753197+georeeen@users.noreply.github.com>
2023-06-14 22:36:55 +0800

    Create index1.php

---
+++ barangay/pages/event/index1.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" href="fullcalendar/fullcalendar.min.css" />
+<script src="fullcalendar/lib/jquery.min.js"></script>
+<script src="fullcalendar/lib/moment.min.js"></script>
+<script src="fullcalendar/fullcalendar.min.js"></script>
+
+<script>
+
+$(document).ready(function () {
+    var calendar = $('#calendar').fullCalendar({
+        editable: false,
+        events: "fetch-event.php",
+        displayEventTime: false,
+        eventRender: function (event, element, view) {
+            if (event.allDay === 'true') {
+                event.allDay = true;
+            } else {
+                event.allDay = false;
+            }
+        },
+    }
+    )}
+    )}
+
+function displayMessage(message) {
+	    $(".response").html("<div class='success'>"+message+"</div>");
+    setInterval(function() { $(".success").fadeOut(); }, 1000);
+}
+</script>
+
+<style>
+body {
+    margin-top: 50px;
+    text-align: center;
+    font-size: 12px;
+    font-family: "Lucida Grande", Helvetica, Arial, Verdana, sans-serif;
+}
+
+#calendar {
+    width: 700px;
+    margin: 0 auto;
+}
+
+.response {
+    height: 60px;
+}
+
+.success {
+    background: #cdf3cd;
+    padding: 10px 60px;
+    border: #c3e6c3 1px solid;
+    display: inline-block;
+}
+</style>
+</head>
+<body>
+    <h1>Barangay Event Manager</h1>
+
+    <div class="response"></div>
+    <div id='calendar'></div>
+</body>
+
+
+</html>
