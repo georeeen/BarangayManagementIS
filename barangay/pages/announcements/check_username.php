fcd222a9dfd3b352e23dce592978542b16ae59b9
georenchlg <94753197+georeeen@users.noreply.github.com>
2023-06-14 22:34:21 +0800

    Delete check_username.php

--- barangay/pages/announcements/check_username.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//mysql query to select field username if it's equal to the username that we check '  
-include "../connection.php";
-$result = mysqli_query($con,"select announcement from tblannounce where announcement = '".$_POST['announcement']."' ");
-$cnt = mysqli_num_rows($result);
-print($cnt);
-?>
